65751f8dc79e7e516518848037281ad4166ed131
Rob Cakebread <rcakebread@mediatemple.net>
2010-11-16 16:02:56 -0800

    Added utils unit tests

---
+++ tests/test_utils.py
@@ -0,0 +1,27 @@
+
+
+
+def insert(original, new, pos):
+    '''Inserts new string inside original string at pos'''
+    return original[:pos] + new + original[pos:]
+
+
+def camel_to_human(original):
+#Index where capital letters are
+    i = 0
+    for s in original:
+        if s.isupper():
+            original = insert(original, " ", i)
+            i += 1
+        i += 1
+    original = original.capitalize()
+    return original
+
+
+def test_camel_to_human():
+    assert camel_to_human("thisWasCamelCase") == "This was camel case"
+    assert camel_to_human("thisIsCamelCase") == "This is camel case"
+    assert camel_to_human("oneTwo") == "One two"
+    assert camel_to_human("oneTwoThree") == "One two three"
+
+
